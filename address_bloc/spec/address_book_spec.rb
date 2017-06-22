ca33b2db9bb6ec495088dec7634d0ac09d9e30d0
Ryan Popovic <rjp203@hotmail.com>
2017-06-22 04:30:48 +0000

    Assignment 4 Completed

--- address_bloc/spec/address_book_spec.rb
+++ address_bloc/spec/address_book_spec.rb
@@ -4,16 +4,19 @@ RSpec.describe AddressBook do
     describe "attributes" do
         it "responds to entries" do
             book = AddressBook.new
+            
             expect(book).to respond_to(:entries)
         end
         
         it "initializes entries as an array" do
             book = AddressBook.new
+            
             expect(book.entries).to be_an(Array)
         end
         
         it "initializes entries as empty" do
             book = AddressBook.new
+            
             expect(book.entries.size).to eq(0)
         end
     end
@@ -34,6 +37,26 @@ RSpec.describe AddressBook do
             expect(new_entry.name).to eq('Ada Lovelace')
             expect(new_entry.phone_number).to eq('010.012.1815')
             expect(new_entry.email).to eq('augusta.king@lovelace.com')
+        end
+    end
+    
+    describe "#remove_entry" do
+        it "removes one entry from the address book" do
+            book = AddressBook.new
+            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            
+            name = 'Tom De La Rocha'
+            phone_number = '112.234.8974'
+            email = 'peopleofthesun@freedom.com'
+            book.add_entry(name, phone_number, email)
+            
+            expect(book.entries.size).to eq(2)
+            
+            book.remove_entry(name, phone_number, email)
+            
+            expect(book.entries.size).to eq(1)
+            
+            expect(book.entries.first.name).to eq('Ada Lovelace')
         end
     end
 end
