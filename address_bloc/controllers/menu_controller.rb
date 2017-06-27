6cd8970d24161922b1529e8b6de88f3bef6fabe7
Ryan Popovic <rjp203@hotmail.com>
2017-06-27 11:47:51 +0000

    Assignment 5 Completed

--- address_bloc/controllers/menu_controller.rb
+++ address_bloc/controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
         puts "2 - Create an entry"
         puts "3 - Search for an entry"
         puts "4 - Import entries from a CSV"
-        puts "5 - Exit"
+        puts "5 - View Entry Number n"
+        puts "6 - Exit"
         print "Enter your selection: "
         
         selection = gets.to_i
@@ -36,12 +37,31 @@ class MenuController
                 read_csv
                 main_menu
             when 5
+                system "clear"
+                view_entry_number_n
+                main_menu
+            when 6
                 puts "Good-bye!"
                 exit(0)
             else
                 system "clear"
-                put "Sorry, that is not a valid input"
+                puts "Sorry, that is not a valid input"
                 main_menu
+        end
+    end
+    
+    def view_entry_number_n
+        print "Which entry # are you looking for? "
+        selection = gets.chomp.to_i
+        
+        if selection <= @address_book.entries.count
+            puts @address_book.entries[selection - 1]
+            puts "Press enter to return to the main menu"
+            gets.chomp
+            system "clear"
+        else 
+            puts "Please enter a valid entry # "
+            view_entry_number_n
         end
     end
 
@@ -102,3 +122,4 @@ class MenuController
         end
     end
 end
+
